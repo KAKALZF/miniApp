f01af5de57cba033fcacce5b9fd575dad4763edb
linzefeng10 <575800503@qq.com>
2026-03-15 10:00:44 +0800

    初始化 Spring Boot Web 项目

---
+++ src/test/java/com/example/miniapp/MiniappApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.miniapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
