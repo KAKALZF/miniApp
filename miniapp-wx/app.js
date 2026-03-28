452304c8a4c3b1f71927c05fa41e437bfbd6fdc9
linzefeng10 <575800503@qq.com>
2026-03-28 10:20:33 +0800

    返回随机内容

--- miniapp-wx/app.js
+++ miniapp-wx/app.js
@@ -1,6 +1,6 @@
 App({
   globalData: {
-    apiBaseUrl: 'http://localhost:8081'
+    apiBaseUrl: 'http://localhost:8083'
   },
   onLaunch() {
     console.log('小程序启动')
--- src/main/java/com/example/miniapp/ApiController.java
+++ src/main/java/com/example/miniapp/ApiController.java
@@ -3,16 +3,31 @@ package com.example.miniapp;
 import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "*")
 public class ApiController {
 
+    private final String[] greetings = {
+        "Hello from Spring Boot! 你好，小程序！",
+        "Welcome to MiniApp! 欢迎使用小程序！",
+        "Have a nice day! 祝你今天愉快！",
+        "Spring Boot is awesome! Spring Boot 真棒！",
+        "Hello World! 世界你好！",
+        "Greetings from the backend! 来自后端的问候！",
+        "Enjoy your coding! 享受编程的乐趣！",
+        "May the code be with you! 愿代码与你同在！"
+    };
+    
+    private final Random random = new Random();
+
     @GetMapping("/hello")
     public Map<String, String> hello() {
         Map<String, String> response = new HashMap<>();
-        response.put("message", "Hello from Spring Boot! 你好，小程序！");
+        String randomGreeting = greetings[random.nextInt(greetings.length)];
+        response.put("message", randomGreeting);
         response.put("timestamp", String.valueOf(System.currentTimeMillis()));
         return response;
     }
